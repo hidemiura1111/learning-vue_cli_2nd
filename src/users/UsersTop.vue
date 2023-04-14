5dbed6d0556f8a73b2122f9ea8fddc29bccd0bd9
hidems <miura.eleven@gmail.com>
2023-04-14 22:55:04 +0200

    multi-select

--- src/users/UsersTop.vue
+++ src/users/UsersTop.vue
@@ -4,14 +4,26 @@
     <a href="index.html">Index Page</a><br>
     <a href="users.html">Users Page</a><br>
     <a href="posts.html">Posts Page</a><br>
+
+    <multiselect v-model="value" :options="options"></multiselect>
   </div>
 </template>
 
 <script>
-export default {
+import Multiselect from 'vue-multiselect'
 
+export default {
+  components: { Multiselect },
+    data () {
+      return {
+        value: null,
+        options: ['user1', 'user2', 'user3']
+      }
+    }
 }
 </script>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
 
 <style>
 
